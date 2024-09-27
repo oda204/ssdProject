804fa436766050909608edeae62c01c535c3c80c
karolineys <karoline.stabell@gmail.com>
2024-09-27 13:44:00 +0200

    fixed getting all .plt files of user and started on inserting activites

--- exercise2/ex2.py
+++ exercise2/ex2.py
@@ -1,6 +1,7 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
 import os
+from datetime import datetime
 
 class ExerciseOneProgram:
     def __init__(self):
@@ -18,7 +19,7 @@ class ExerciseOneProgram:
         activity_table = """CREATE TABLE IF NOT EXISTS ACTIVITY (
             id INT NOT NULL,
             user_id INT NOT NULL,
-            transportation_mode TEXT,
+            transportation_mode TEXT DEFAULT NULL,
             start_date_time DATETIME,
             end_date_time DATETIME,
             PRIMARY KEY (id),
@@ -66,9 +67,62 @@ class ExerciseOneProgram:
         # self.db_connection.commit()
 
     def insert_data_activity(self):
-        query = "INSERT INTO USER (id, has_labels) VALUES (010, TRUE);"
+        folder_files_dict = {}
 
-        self.cursor.execute(query)
+        # Traverse the main directory and its immediate subdirectories
+        for root, dirs, files in os.walk(self.data_path + "/Data"):
+            # Collect file names in the main directory (root level)
+            folder_files_dict[os.path.basename(root)] = files
+            
+            # Now, for each immediate subdirectory, gather file names
+            for folder in dirs:
+                folder_path = os.path.join(root, folder)
+                for sub_root, sub_dirs, sub_files in os.walk(folder_path):
+                    # Add the subfolder and its associated file names to the dictionary
+                    folder_files_dict[folder] = sub_files
+                # No need to go deeper into nested subdirectories
+                
+            
+            # Stop after the first level to avoid recursion
+            break
+        
+        folder_files_dict.pop("Data")
+
+        for key, value in folder_files_dict.items():
+            for file_name in value:
+                if not file_name.endswith(".txt"):
+                    file_path = os.path.join(self.data_path + "/Data", key, "Trajectory", file_name)
+                    with open(file_path, "r") as file:
+                        lines = file.readlines()
+                    if len(lines) <= 2506:
+                        start_day = lines[6].split(",")[-2].replace('/', '-').strip()
+                        start_time = lines[6].split(",")[-1].strip()
+            
+                        end_day = lines[-1].split(",")[-2].replace('/', '-').strip()
+                        end_time = lines[-1].split(",")[-1].strip()
+
+                        start_datetime_str = f"{start_day} {start_time}"
+                        end_datetime_str = f"{end_day} {end_time}"
+
+                        # Convert to a datetime object
+                        start_time = datetime.strptime(start_datetime_str, '%Y-%m-%d %H:%M:%S')
+                        end_time = datetime.strptime(end_datetime_str, '%Y-%m-%d %H:%M:%S')
+                        
+                    
+
+                        id = int(key + file_name.split(".")[0])
+                        user_id = int(key)
+
+                        print(id, user_id, start_time, end_time)
+                        query = "INSERT INTO ACTIVITY (id, user_id, start_date_time, end_date_time) VALUES (%s, %s, %s, %s);"
+                        self.cursor.execute(query, (id, user_id, start_time, end_time))
+                    break
+
+
+
+    
+
+        # self.cursor.execute(query)
     
     def fetch_data(self, table_name):
         query = "SELECT * FROM %s"
@@ -99,7 +153,9 @@ def main():
         program = ExerciseOneProgram()        
         program.create_tables()
         program.insert_data_user()
+        program.insert_data_activity()
         _ = program.fetch_data(table_name="USER")
+        _ = program.fetch_data(table_name="ACTIVITY")
         # program.drop_table(table_name="User")
         # Check that the table is dropped
         program.show_tables()
